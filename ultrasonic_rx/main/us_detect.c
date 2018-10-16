ad40276fa0c3b003b1369dadad6aa852d3780b79
Jeroen Domburg <git@j0h.nl>
2018-10-16 22:49:21 +0800

    New notes, dump US bit

--- ultrasonic_rx/main/us_detect.c
+++ ultrasonic_rx/main/us_detect.c
@@ -13,9 +13,8 @@
 //Does an approximation of the magnitude of the vector [x,y], normally
 //calculated using sqrt(x^2+y^2). See Wikipedia for more info:
 //https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
-//We use 61/64 for alpha and 3/8 (=24/64) for beta.
 int magnitude_est(int x, int y) {
-	const int alpha=61, beta=24, div=64;
+	const int alpha=30, beta=15, div=32;
 	if (x<0) x=-x;
 	if (y<0) y=-y;
 	//use x as max, y as min
@@ -29,14 +28,18 @@ int magnitude_est(int x, int y) {
 	return (alpha*x+beta*y)/div;
 }
 
-static int64_t us_start_time=0;
-static int64_t us_ping_start=0;
-static int64_t us_tot_samples=0;
+void flush_i2s() {
+	size_t sl;
+	uint16_t samp;
+	do {
+		i2s_read(0, (void*)&samp, 2, &sl, 0);
+	} while (sl==2);
+}
 
 void us_init() {
 	 i2s_config_t i2s_config = {
 		.mode = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN,
-		.sample_rate =	(40000*8)/16, //40KHz input signal, sample 8 times per input signal cycle
+		.sample_rate =	(40500*8)/16, //40KHz input signal, sample 8 times per input signal cycle
 		.bits_per_sample = 16,
 		.communication_format = I2S_COMM_FORMAT_I2S_MSB,
 		.channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
@@ -47,16 +50,25 @@ void us_init() {
 	};
 	//install and start i2s driver
 	i2s_driver_install(0, &i2s_config, 0, NULL);
-	us_start_time=esp_timer_get_time();
 	//init ADC pad
 	i2s_set_adc_mode(ADC_UNIT_1, ADC1_CHANNEL_0);
 	i2s_adc_enable(0);
+	i2s_stop(0);
+	flush_i2s();
 }
 
 
 
+volatile int running=0;
+
+//int64_t lastping;
+
 void us_mark_start() {
-	us_ping_start=esp_timer_get_time();
+//	int64_t nowping=esp_timer_get_time();
+	if (!running) i2s_start(0);
+	running=1;
+//	printf("Ping delay %d\n", (int)(nowping-lastping));
+//	lastping=nowping;
 }
 
 //Moving average over hlen samples
@@ -80,95 +92,127 @@ static inline void mov_avg_filter(int hlen, int16_t *samps, int samplen) {
 }
 
 
-#define SNIFF_LEN 256
-#define PARSE_LEN 1024
+#define PARSE_LEN 2048
 
 
 void us_run() {
 	uint16_t *samp;
-	int16_t *ival, *qval, *mul;
+	int16_t *ival, *qval, *mul, *devv;
 	samp=calloc(sizeof(uint16_t), PARSE_LEN);
 	ival=calloc(sizeof(int16_t), PARSE_LEN);
 	qval=calloc(sizeof(int16_t), PARSE_LEN);
 	mul=calloc(sizeof(int16_t), PARSE_LEN);
+	devv=calloc(sizeof(int16_t), PARSE_LEN);
 	
+//	const int8_t rphase[]={0,1,0,0,1,1,1}; //Note: each phase is held for 4 40KHz cycles
+	const int8_t rphase[]={1,1,0,0}; //Note: each phase is held for 4 40KHz cycles
+	const int rphase_samps=(8*8); //samples per one rphase entry
+	const int rphase_len=sizeof(rphase)*rphase_samps;
+	const int sinmul[8]={0, 5, 7, 5, 0, -5, -7, -5};
+
 	while(1) {
-		size_t l=0, sl=0;
-		//Read some samples, see if there's activity
-		while(1) {
-			i2s_read(0, (void*)samp, SNIFF_LEN*2, &sl, portMAX_DELAY);
-			sl=sl/sizeof(int16_t); //to samples
-			//This is a very quick and dirty qam demodulation and averaging, just to see if there's any
-			//40KHz signal in the sample we took.
-			int ri=0, rq=0;
-			for (int i=0; i<sl; i++) {
-				if ((i+0)&4) ri+=samp[i]; else ri-=samp[i];
-				if ((i+2)&4) rq+=samp[i]; else rq-=samp[i];
-			}
-			int m=magnitude_est(ri, rq);
+		size_t l=0;
+		//Read some samples
+		i2s_read(0, (void*)samp, PARSE_LEN*2, &l, portMAX_DELAY);
 
-			if (m>1000) {
-				printf("Tresh %d\n", m);
-				break;
-			} else {
-				//Clean bitbucket, try again.
-				us_tot_samples+=sl;
-			}
+#if 0
+		//Fake sig
+		for (int i=0; i<rphase_len; i++) {
+			int m=10;
+			if (rphase[i/rphase_samps]) m=-10;
+			samp[(i+1000)^1]+=(m*sinmul[(i+7)&7]);
 		}
-		//Read the rest of the samples
-		i2s_read(0, (void*)&samp[sl], (PARSE_LEN-sl)*2, &l, portMAX_DELAY);
+#endif
 
-		l=(l/sizeof(int16_t))+sl; //bytes -> samples, add sniffed len as well
+		l=l/sizeof(int16_t); //bytes -> samples
 		//QAM-demodulate the signal 
 		int sp=0, cp=2;
-		const int sinmul[8]={0, 5, 7, 5, 0, -5, -7, -5};
 		for (int i=0; i<l; i++) {
 			//I is incoming signal * sin(40KHz), Q is incoming signal * cos(40KHz).
 			//Because the sample freq is a nice multiple of the carrier freq, we hardcode the sine/cosine here.
-			ival[i]=((int)samp[i]*sinmul[sp&7])/8;
-			qval[i]=((int)samp[i]*sinmul[cp&7])/8;
+			ival[i]=((int)samp[i^1]*sinmul[sp&7])/8;
+			qval[i]=((int)samp[i^1]*sinmul[cp&7])/8;
 			sp++; cp++;
 		}
+
+
 
 		//Filter to get the envelope of the i/q values
 		mov_avg_filter(8, ival, l);
 		mov_avg_filter(8, qval, l);
 
+		int start=0;
+		int val_samps=0;
+		for (start=0; start<l; start++) {
+			if (magnitude_est(ival[start], qval[start])>4) val_samps++;
+			if (val_samps>100) {
+				start-=100;
+				break;
+			}
+		}
+
+
 		//We're only interested in the phase of the signal, not the amplitude, so we normalize the I and Q streams
 		//if they have any amplitude at all.
-#if 0
+#if 1
 		for (int i=0; i<l; i++) {
 			int amp=magnitude_est(ival[i], qval[i]);
-			if (amp>8) {
+			if (amp>2) {
 				ival[i]=(((int)ival[i])<<8)/amp;
 				qval[i]=(((int)qval[i])<<8)/amp;
+			} else {
+				ival[i]=0; qval[i]=0;
 			}
 		}
 #endif
 
+		//There's no need to do cross-correlation ot a range that has no amplitude; pre-process the mul array
+		//to filter those out.
+		int skip=rphase_len;
+		int found_zeroes=0;
+		for (int i=l-1; i>=0; i--) {
+			if (ival[i]+qval[i]==0) found_zeroes++; else found_zeroes=0;
+			if (found_zeroes>3) skip=rphase_len*0.5;
+			if (skip) skip--;
+			mul[i]=skip?0:1;
+		}
+
+		//We expect the cross-correlation to be between 0 and 600 samples from the start of amplitude ramp-up.
+		int valid_samps=0;
+		for (int i=0; i<l; i++) {
+			if (mul[i]) {
+				valid_samps++;
+				if (valid_samps>230) mul[i]=0;
+			} else {
+				valid_samps=0;
+			}
+		}
+
+
 		//Do correlation with the known pseudo-random ping M-sequence
 		//http://www.commsp.ee.ic.ac.uk/~mrt102/projects/mls/MLS%20Theory.pdf for some related info
-		const int8_t rphase[]={1,-1,1,1,-1,-1,-1}; //Note: each phase is held for 4 40KHz cycles
-		const int rphase_samps=(8*4); //samples per one rphase entry
 		for (int i=0; i<l-sizeof(rphase)*rphase_samps; i++) {
 			int ci, cq;
 			ci=0; cq=0;
-			for (int j=0; j<sizeof(rphase)*rphase_samps; j+=rphase_samps) {
-					if (rphase[j/rphase_samps]==1) {
+			if (mul[i]) {
+				for (int j=0; j<sizeof(rphase)*rphase_samps; j+=rphase_samps) {
+					if (rphase[j/rphase_samps]==0) {
 						for (int k=0; k<rphase_samps; k++) {
 							ci+=ival[i+j+k];
 							cq+=qval[i+j+k];
 						}
 					} else {
 						for (int k=0; k<rphase_samps; k++) {
-							ci-=ival[i+j];
-							cq-=qval[i+j];
+							ci-=ival[i+j+k];
+							cq-=qval[i+j+k];
 						}
+					}
 				}
 			}
-			mul[i]=magnitude_est(ci, cq);
+			int m=magnitude_est(ci, cq)/(rphase_samps*2);
+			mul[i]=m;
 		}
-//		for (int i=0; i<l; i++) printf("%d ", mul[i]);
+
 
 		//Find max value
 		int max=0;
@@ -176,11 +220,50 @@ void us_run() {
 			if (mul[j]>mul[max]) max=j;
 		}
 
-		uint64_t timestamp=((us_tot_samples+max)*1000000ULL)/(320000ULL)+us_start_time;
-		if (mul[max]>300) printf("Found: at %d max %d, %d us\n", max, mul[max], (int)(timestamp-us_ping_start));
-		us_tot_samples+=l;
-		int sps=(((esp_timer_get_time()-us_start_time)*320)/1000);
-		printf("Samps %d should be %d diff %d\n", (int)us_tot_samples, sps, sps-(int)us_tot_samples);
+		const int delta_avg=10;
+		int pos_run=0, neg_run=0, prevc=0, dmax=0, dmaxpos=0;
+		for (int i=delta_avg; i<l-delta_avg; i++) {
+			int c;
+			c=0;
+			if (mul[i]) {
+				for (int j=-delta_avg; j<=delta_avg; j++) {
+					c=c+j*mul[i+j];
+				}
+			}
+			devv[i]=c/4;
+
+			if (c>0) pos_run++;
+			if (c<0) neg_run++;
+			if (prevc==0 && c==0) {
+				pos_run=0;
+				neg_run=0;
+			}
+			if (prevc<0 && c>0) {
+				int tot=pos_run+neg_run;
+				if (tot>dmax) {
+					dmax=tot;
+					dmaxpos=i-neg_run;
+				}
+				pos_run=0; neg_run=0;
+			}
+			prevc=c;
+
+		}
+
+
+		if (mul[max]>200) {
+			printf("Found: start %d at %d max %d found_div %d div_run %d\n", start, max, mul[max], dmaxpos, dmax);
+
+	if (start<100) {
+			for (int i=0; i<l; i++) {
+				printf("%d %d %d %d %d %d\n", i, samp[i^1], ival[i], qval[i], mul[i], devv[i]);
+			}
+	}
+		}
+
+		i2s_stop(0);
+		flush_i2s();
+		running=0;
 
 	}
 }
--- ultrasonic_rx/plotdata.sh
+++ ultrasonic_rx/plotdata.sh
@@ -1,8 +1,13 @@
 gnuplot <<EOF
 #set terminal x11
+mag(x,y)=sqrt(x*x+y*y)
 plot "$1" using 1:2 with lines, \
 	"$1" using 1:3 with lines, \
 	"$1" using 1:4 with lines, \
-	"$1" using 1:(atan2(\$4,\$3)*400) lw 2 with lines
+	"$1" using 1:5 lw 2 with lines, \
+	"$1" using 1:(mag(\$3,\$4)) with lines, \
+	"$1" using 1:6 lw 2 with lines
 pause 30000
 EOF
+
+#	"$1" using 1:(atan2(\$4,\$3)*800) with lines
